a3882276b9759cb98f9157dd44d61a8dad460427
Michael <michael138leighton@gmail.com>
2022-05-09 16:29:31 -0500

    rounded value

--- script.js
+++ script.js
@@ -32,23 +32,28 @@ function areaRect(){
 	const width = document.getElementById('width').valueAsNumber
 	const height = document.getElementById('height').valueAsNumber
 	const area = width * height
+	const rounded = Math.round(area)
 	document.getElementById('area').textContent = area
+	document.getElementById('round-rect').textContent = rounded
 }
 
 function areaWD(){
 	const diameter = document.getElementById('diameter').valueAsNumber
 	const area = diameter * Math.PI
+	const rounded = Math.round(area)
 	document.getElementById('area-w-d').textContent = area
 }
 
 function areaWR(){
 	const radius = document.getElementById('radius').valueAsNumber
 	const area = radius * radius * Math.PI
+	const rounded = Math.round(area)
 	document.getElementById('area-w-r').textContent = area
 }
 
 function areaWC(){
 	const circum = document.getElementById('circum').valueAsNumber
 	const area = (circum * circum) / (4 * Math.PI)
+	const rounded = Math.round(area)
 	document.getElementById('area-w-c').textContent = area
 }
